bc639dbf5843a79bbeaca85c1b9fa6c40e274293
Stanley Huang <huang.stanley050@gmail.com>
2019-08-05 14:57:09 +1000

    verify that firebase admin verifyIdtoken function works with token

--- src/app.js
+++ src/app.js
@@ -5,20 +5,20 @@ import logger from "morgan";
 import * as admin from "firebase-admin";
 import serviceAccount from "./pwagram-bd625-firebase-adminsdk-ew3ye-863e79e816.json";
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
 import authRouter from "./routes/auth";
+import resourceRouter from "./routes/resource";
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+const auth = admin.auth();
 const app = express();
-
+app.set("auth", auth);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use("/", indexRouter);
+
 app.use("/api/auth", authRouter);
-app.use("/users", usersRouter);
+app.use("/api/resource", resourceRouter);
 
 export default app;
