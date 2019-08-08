218077b0f4c6613d03c551143917cf1d1b75febc
Stanley Huang <huang.stanley050@gmail.com>
2019-08-08 16:43:13 +1000

    able to fetch data from giphy, can save the post to firebase with complete info

--- src/controllers/resource.js
+++ src/controllers/resource.js
@@ -1,4 +1,5 @@
 import { checkToken } from "../middlewares/checkToken";
+import GphApiClient from "giphy-js-sdk-core";
 import formatTime from "../middlewares/formatTime";
 const old_token =
   "eyJhbGciOiJSUzI1NiIsImtpZCI6IjcyODRlYTZiNGZlZDBmZDc1MzE4NTg2NDZmZDYzNjE1ZGQ3YTIyZjUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcHdhZ3JhbS1iZDYyNSIsImF1ZCI6InB3YWdyYW0tYmQ2MjUiLCJhdXRoX3RpbWUiOjE1NjQ5ODA2ODksInVzZXJfaWQiOiJwN1E2bDY5bjEzUnFkaXRpbHRhOE9JTkRGTEEzIiwic3ViIjoicDdRNmw2OW4xM1JxZGl0aWx0YThPSU5ERkxBMyIsImlhdCI6MTU2NDk4MDY4OSwiZXhwIjoxNTY0OTg0Mjg5LCJlbWFpbCI6ImluZmFtb3VzX2dvZGhhbmRAeWFob28uY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImluZmFtb3VzX2dvZGhhbmRAeWFob28uY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.RpFWKZMDqVH6G0Y2kiF8iWo3ouo9PGk7u9qo29yGNA0gyZUCBNwu4BhRyyBGw5kcaPZXg0ywjAGZ2QzC3U0oND16bNTyHwAhM-rpGZOKQ3_29ZE9E1UXHSnCZ0hyVGQdEwXTbXM9cmnnvDqgl7HGIFdbrlgZQmmDVj_vYbXUGnlxW-oh9FMZGw7JYuE9WTaGztg3MSkGOcWV1BquuHJgGe-eMuA6sk1yrUKHxyxfXmDmu_ocshWXicq_TJ0jWZ1Vz9KOYT_FPK7UHZSxcl2HBbybO1v898qRp_FEREpw4gKN66w0nVjxKL5ixxuvtf3J1_lo3FThvnriW_1S_QTOrQ";
@@ -24,6 +25,10 @@ export default {
     const admin = req.app.get("auth");
     const token = req.token;
     const timestamp = req.params.timestamp;
+    const mood = req.body.mood;
+    const description = req.body.description;
+    let firebaseData;
+    let url;
     let check_result;
 
     check_result = await checkToken(admin, token);
@@ -35,14 +40,28 @@ export default {
     }
     const dayMade = formatTime();
 
-    const data = {
-      userId: "123456",
-      dayMade,
-      timestamp,
-      gifyUrl: "www.textdd.com",
-      mood: "wicked"
+    let client = GphApiClient(process.env.GIPHY_KEY);
+    let giphyResult;
+    const giphyParams = {
+      tag: mood,
+      rating: "g"
     };
-    let save_result = await db.collection("moods").add(data);
+    try {
+      giphyResult = await client.random("gifs", giphyParams);
+      url = giphyResult.data.url;
+    } catch (err) {
+      console.log(err);
+    }
+
+    firebaseData = {
+      uid: "128a",
+      dayMade,
+      gifUrl: giphyResult.data.url,
+      mood,
+      description
+    };
+
+    await db.collection("moods").add(firebaseData);
 
     res.json({ message: "route to store mood to database" });
   }
